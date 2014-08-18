60418fbd6d35af3c43aef94340a2ac0dbfd8d0dc
Reuel <reuel_sc@live.com>
2014-08-17 22:54:05 -0300

    alteração realizada

---
+++ coordenador/cadastroUsuario.php
@@ -0,0 +1,221 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Cadastro de Aluno</title>
+
+	<link rel="stylesheet" href="../css/bootstrap.css">
+	<link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+
+	<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+		<div class="container container-2">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
+					<span class="sr-only">Toggle Navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="">Logo</a>
+			</div>
+
+			<div class="collapse navbar-collapse" id="bs-navbar-collapse-1">
+				<ul class="nav navbar-nav navbar-right">
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> Usuário <span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Sub1</a></li>
+							<li><a href="#">Sub2</a></li>
+							<li><a href="#">Sair <span class="pull-right glyphicon glyphicon-log-out"></span></a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div class="wrapper" role="main">
+		<div class="container container-2">
+			<div class="row affix-row">
+				<div id="menu" class="col-xs-12 col-sm-3 col-md-2 affix-sidebar">
+					<div class="sidebar-nav">
+						<div class="navbar navbar-default">
+							<div id="navbar-collapse-1" class="navbar-collapse collapse">
+								<ul class="nav navbar-nav">
+									<li class="active">
+										<a href="#" data-toggle="collapse"><h4>Menu</h4></a>
+									</li>
+
+									<li>
+										<a href="#" data-toggle="collapse" data-target="#toggleSidebar" data-parent="#sidenav01" class="collapsed">
+											<span>Submenu 1 <span class="caret pull-right"></span></span>
+										</a>
+
+										<div class="collapse" id="toggleSidebar" style="height: 0px;">
+											<ul class="nav nav-list">
+												<li><a href="#">Submenu 1.1</a></li>
+												<li><a href="#">Submenu 1.2</a></li>
+												<li><a href="#">Submenu 1.3</a></li>
+											</ul>
+										</div>
+									</li>
+
+									<li>
+										<a href="#" data-toggle="collapse" data-target="#toggleSidebar2" data-parent="#sidenav02" class="collapsed">
+											<span>Submenu 2 <span class="caret pull-right"></span></span>
+										</a>
+
+										<div class="collapse" id="toggleSidebar2" style="height: 0px;">
+											<ul class="nav nav-list">
+												<li><a href="#">Submenu 2.1</a></li>
+												<li><a href="#">Submenu 2.2</a></li>
+												<li><a href="#">Submenu 2.3</a></li>
+											</ul>
+										</div>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div id="conteudo" class="col-xs-12 col-sm-9 col-md-10 affix-content">
+					<div class="container container-2">
+						<div class="page-header">
+							<h3><span class="glyphicon glyphicon-th-list"></span> Cadastro de Usuário</h3>
+						</div>
+
+						<form method="post" action="" class="form-horizontal" role="form">
+							<div class="row">
+								<div class="col-md-8">
+									<div class="form-group">
+										<label for="inputName" class="col-xs-6 col-sm-2 col-md-1 col-lg-2 control-label">Nome:</label>
+										<div class="col-xs-6 col-sm-10 col-md-11 col-lg-10">
+											<input type="text" class="form-control" id="inputName" required>
+										</div>
+									</div>
+
+									<div class="form-group">
+										<label for="radioPerfil" class="col-xs-6 col-sm-2 col-md-1 col-lg-2 control-label">Perfil:</label>
+										<div class="col-xs-6 col-sm-10 col-md-11 col-lg-10">
+											<label class="radio-inline">
+												<input type="radio" name="optionPerfil" id="optionProf" value="professor">Professor</input>
+											</label>
+
+											<label class="radio-inline">
+												<input type="radio" name="optionPerfil" id="optionCoord" value="coordenador">Coordenador</input>
+											</label>
+
+											<label class="radio-inline">
+												<input type="radio" name="optionPerfil" id="optionSecret" value="secretaria">Secretária</input>
+											</label>
+										</div>
+									</div>
+
+									<div class="form-group">
+										<label for="inputCPF" class="col-xs-6 col-sm-2 col-md-1 col-lg-2 control-label">CPF:</label>
+										<div class="col-xs-6 col-sm-10 col-md-11 col-lg-10">
+											<input type="text" class="form-control" id="inputCPF" required>
+										</div>
+									</div>
+
+									<div class="form-group">
+										<label for="inputNasc" class="col-xs-6 col-sm-2 col-md-1 col-lg-2 control-label">Data de Nascimento:</label>
+										<div class="col-xs-6 col-sm-10 col-md-11 col-lg-10">
+											<input type="text" class="form-control" id="inputNasc" required>
+										</div>
+									</div>
+
+									<div class="form-group">
+										<label for="inputPhone" class="col-xs-6 col-sm-2 col-md-1 col-lg-2 control-label">Telefone:</label>
+										<div class="col-xs-6 col-sm-10 col-md-11 col-lg-10">
+											<input type="text" class="form-control" id="inputPhone" required>
+										</div>
+									</div>
+
+									<div class="form-group">
+										<label for="inputEndereco" class="col-xs-6 col-sm-2 col-md-1 col-lg-2 control-label">Endereço:</label>
+										<div class="col-xs-6-col-sm-10 col-md-11 col-lg-10">
+											<input type="text" class="form-control" id="inputEndereco" required>
+										</div>
+									</div>
+
+									<div class="form-group">
+										<label for="inputComplemento" class="col-xs-6 col-sm-2 col-md-1 col-lg-2 control-label">Complemento:</label>
+										<div class="col-xs-6-col-sm-10 col-md-11 col-lg-10">
+											<input type="text" class="form-control" id="inputComplemento" required>
+										</div>
+									</div>
+
+									<div class="form-group">
+										<label for="inputCidade" class="col-xs-6 col-sm-2 col-md-1 col-lg-2 control-label">Cidade:</label>
+										<div class="col-xs-6-col-sm-10 col-md-11 col-lg-10">
+											<input type="text" class="form-control" id="inputCidade" required>
+										</div>
+									</div>
+
+									<div class="form-group">
+										<label for="optionEstado" class="col-xs-6 col-sm-2 col-md-1 col-lg-2 control-label">Estado:</label>
+										<div class="col-xs-6 col-sm-10 col-md-11 col-lg-10">
+											<select name="selectEstado" id="selectEstado" class="form-control" required>
+												<option></option>
+												<option>AM</option>
+												<option>PA</option>
+												<option>BA</option>
+												<option>TO</option>
+												<option>DF</option>
+												<option>MA</option>
+												<option>CE</option>
+												<option>RJ</option>
+												<option>SP</option>
+											</select>
+										</div>
+									</div>
+
+									<div class="pull-right">
+										<button type="button" class="btn btn-success">Cadastrar</button>
+										<button type="button" class="btn btn-warning">Editar</button>
+										<button type="button" class="btn btn-danger">Excluir</button>
+									</div>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<footer>
+		<div class="container container-2">
+			<div class="row">
+				<div class="col-xs-12 col-sm-3 col-md-6">
+					<h5><p>Anhanguera Educacional Faculdade de Negócios e Tecnologia da Informação - FACNET<br />Fone: (61) 0000-0000 Fax: (61) 0000-0000<br />Taguatinga-DF</p></h5>
+				</div>
+						
+				<div id="logoFooter" class="col-xs-12 col-sm-3 col-md-3 col-md-offset-3 col-md-3">
+					<h2>Logo</h2>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+	<div class="copyright">
+		<div class="container container-2">
+			<div class="row">
+				<div class="col-md-12">
+					<p>&copy; Todos os direitos reservados</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script src="../js/jquery.min.js"></script>
+	<script src="../js/bootstrap.min.js"></script>
+</body>
+</html>
