89e19e02d423597325809e71b368101112aa4f1a
Reuel <reuel_sc@live.com>
2014-08-24 02:59:20 -0300

    adicionado novos arquivos

---
+++ coordenador/alunos.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Alunos</title>
+
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/style.css">
+
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+
+	<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+		<div class="container-fluid container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="sr-only">Toggle Navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a href="#" class="navbar-brand">{Logo}</a>
+			</div>
+
+			<div class="collapse navbar-collapse" id="navbar-collapse">
+				<ul class="nav navbar-nav">
+					<li><a href="#">Menu 1</a></li>
+					<li><a href="#">Menu 2</a></li>
+					<li><a href="#">Menu 3</a></li>
+					<li><a href="#">Menu 4</a></li>
+				</ul>
+
+				<ul class="nav navbar-nav navbar-right">
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> Usuário <span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Sub1</a></li>
+							<li><a href="#">Sub2</a></li>
+							<li><a href="#">Sair <span class="glyphicon glyphicon-log-out"></span></a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div class="wrapper" role="main">
+		<div class="container-fluid container">
+			<div class="row">
+				<div id="conteudo" class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+					<div class="page-header">
+						<h3><span class="glyphicon glyphicon-th-list"></span> Lista de Alunos</h3>
+					</div>
+
+					<div class="row">
+						<div class="col-md-10">
+							<button type="button" class="btn btn-success">Cadastrar Aluno</button>
+						</div>
+
+						<div class="col-md-2">
+							<ul class="pagination">
+								<li><a href="#">&laquo;</a></li>
+								<li><a href="#">1</a></li>
+								<li><a href="#">2</a></li>
+								<li><a href="#">&raquo;</a></li>
+							</ul>
+						</div>
+					</div>
+
+					<div class="clearfix"></div>
+
+					<div class="table-responsive">
+						<table class="table table-striped bordered">
+							<thead class="h4">
+								<tr>
+									<th id="name">
+										<span id="nameHeader" style="cursor:pointer;">Nome</span>
+										<span id="nameCaret" class="caret" style="display:none;"></span>
+										<span id="namePesquisarIcon" class="glyphicon glyphicon-search"></span>
+										<div id="namePesquisar" class="hide">
+											<div class="input-group">
+												<input type="text" id="nameInput" class="form-control" />
+												<span class="input-group-btn">
+													<button id="nameApagarPesquisa" class="btn btn-default" type="button" onclick="removerFiltro('nome')"><span class="glyphicon glyphicon-remove"></span></button>
+												</span>
+											</div>
+										</div>
+										<span id="nameApagarPesquisaIcon" class="glyphicon glyphicon-remove-sign" style="display:none;"></span>
+									</th>
+									<th>Ação</th>
+								</tr>
+							</thead>
+							<tbody class="h5">
+								<tr>
+									<td>Reuel Scarabele Cardoso</td>
+									<td><button type="button" class="btn btn-warning btn-xs">Editar</button> <button type="button" class="btn btn-danger btn-xs" data-toggle="modal" data-target="#modal-delete">Excluir</button></td>
+									<div class="modal fade" id="modal-delete" tabindex="-1" role="dialog" aria-labelledby="modal-delete" aria-hidden="true">
+										<div class="modal-dialog modal-sm">
+											<div class="modal-content">
+												<div class="modal-header">
+													<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Sair</span></button>
+												</div>
+												<div class="modal-body">
+													<p>Tem certeza que deseja remover esse aluno?</p>
+												</div>
+												<div class="modal-footer">
+													<button type="button" class="btn btn-default" data-dismiss="modal">Não</button>
+													<button type="button" class="btn btn-danger">Sim</button>
+												</div>
+											</div>
+										</div>
+									</div>
+								</tr>
+							</tbody>
+							<tbody class="h5">
+								<tr>
+									<td>Reuel Scarabele Cardoso</td>
+									<td><button type="button" class="btn btn-warning btn-xs">Editar</button> <button type="button" class="btn btn-danger btn-xs">Excluir</button></td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+
+					<div class="row">
+						<div class="col-md-offset-10 col-md-2">
+							<ul class="pagination">
+								<li><a href="#">&laquo;</a></li>
+								<li><a href="#">1</a></li>
+								<li><a href="#">2</a></li>
+								<li><a href="#">&raquo;</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<footer class="navbar navbar-fixed-bottom">
+		<div class="copyright">
+			<div class="container container-fluid">
+			  <div class="row">
+				<div class="col-md-12">
+				  <p>&copy; Todos os direitos reservados</p>
+				</div>
+			  </div>
+			</div>
+		</div>
+	</footer>
+	
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
