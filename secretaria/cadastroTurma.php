b2be91c48ebca8f8328a6b1697f5a810425791fb
Reuel <reuel_sc@live.com>
2014-08-24 02:57:37 -0300

    arrumando formulario de cadastro de turma

--- secretaria/cadastroTurma.php
+++ secretaria/cadastroTurma.php
@@ -9,7 +9,7 @@
 
 	<link rel="stylesheet" href="../css/bootstrap.css">
 	<link rel="stylesheet" href="../css/style.css">
-	<link rel="stylesheet" href="../css/datepicker.css">
+	<link rel="stylesheet" href="../css/bootstrap-datetimepicker.min.css">
 
 	<!--[if lt IE 9]>
 		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
@@ -19,33 +19,148 @@
 <body>
 
 	<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-		<div class="container container-2">
+		<div class="container-fluid container">
 			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
 					<span class="sr-only">Toggle Navigation</span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="">Logo</a>
+				<a href="#" class="navbar-brand">{Logo}</a>
 			</div>
 
-			<div class="collapse navbar-collapse" id="bs-navbar-collapse-1">
-				<div class="nav navbar-nav navbar-right">
+			<div class="collapse navbar-collapse" id="navbar-collapse">
+				<ul class="nav navbar-nav">
+					<li><a href="../secretaria/index.php">Home</a></li>
+					<li><a href="../secretaria/alunos.php">Alunos</a></li>
+					<li><a href="../secretaria/turmas.php">Turmas</a></li>
+					<li><a href="../secretaria/disciplinas.php">Disciplinas</a></li>
+				</ul>
+
+				<ul class="nav navbar-nav navbar-right">
 					<li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> Usuário <span class="caret"></span></a>
 						<ul class="dropdown-menu">
 							<li><a href="#">Sub1</a></li>
+							<li class="divider"></li>
 							<li><a href="#">Sub2</a></li>
+							<li class="divider"></li>
 							<li><a href="#">Sair <span class="pull-right glyphicon glyphicon-log-out"></span></a></li>
 						</ul>
 					</li>
-				</div>
+				</ul>
 			</div>
 		</div>
 	</nav>
 
 	<div class="wrapper" role="main">
+		<div class="container container-fluid">
+			<div class="row">
+				<div id="conteudo" class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+					<div class="page-header">
+						<h3><span class="glyphicon glyphicon-th-list"></span> Cadastro de Turma</h3>
+					</div>
+
+					<form method="post" action="" class="form-horizontal" role="form">
+						<div class="row">
+							<div class="col-md-8">
+								<div class="form-group">
+									<label for="inputName" class="col-xs-6 col-sm-2 col-md-1 col-lg-2 control-label">Nome:</label>
+									<div class="col-xs-6 col-sm-10 col-md-11 col-lg-10">
+										<input type="text" class="form-control" id="inputName" required>
+									</div>
+								</div>
+
+								<div class="well">
+									<div id="datepicker" class="input-append">
+										<input type="text" data-format="dd/MM/yyyy">
+										<span class="add-on">
+											<i data-time-picker="glyphicon glyphicon-time" data-date-icon="glyphicon glyphicon-calendar"></i>
+										</span>
+									</div>
+								</div>
+
+								<!--<div class="form-group">
+									<label for="Date" class="col-xs-6 col-sm-2 col-md-1 col-lg-2 control-label">Data:</label>
+									<div class="col-xs-6 col-sm-10 col-md-11 col-lg-10">
+										<div class="input-group date">
+											<input type="text" class="form-control">
+											<span class="input-group-addon">
+												<i class="glyphicon glyphicon-calendar"></i>
+											</span>
+										</div>
+									</div>
+								</div>-->
+
+								<div class="form-group">
+									<label for="Hora" class="col-xs-6 col-sm-2 col-md-1 col-lg-2 control-label">Hora:</label>
+									<div class="col-xs-6 col-sm-10 col-md-11 col-lg-10">
+										<div class="input-group time">
+											<input type="text" class="form-control" />
+											<span class="input-group-addon">
+												<span class="glyphicon glyphicon-time"></span>
+											</span>
+										</div>
+									</div>
+								</div>
+
+								<hr />
+
+								<div class="form-group">
+									<label for="inputDisponibilidade" class="col-xs-6 col-sm-2 col-md-1 col-lg-2 control-label">Disponibilidade:</label>
+									<div class="col-xs-6 col-sm-10 col-md-11 col-lg-10">
+										<select name="selectDisponibilidade" id="selectDisponibilidade" class="form-control" required>
+											<option></option>
+											<option>Guilherme Costa</option>
+											<option>Fernando Gonçalves</option>
+											<option>Wesley Tschiedel</option>
+											<option>Welton Dias</option>
+										</select>
+									</div>
+								</div>
+
+								<div class="form-group">
+									<label for="selectDependencia" class="col-xs-6 col-sm-2 col-md-1 col-lg-2 control-label">Dependência:</label>
+									<div class="col-xs-6-col-sm-10 col-md-11 col-lg-10">
+										<select name="selectDependencia" id="selectDependencia" class="form-control" required>
+											<option value=""></option>
+											<option value="rdr">RDR</option>
+											<option value="str">STR</option>
+											<option value="gtr">GTR</option>
+											<option value="trt">TRT</option>
+											<option value="ster">STER</option>
+										</select>
+									</div>
+								</div>
+
+								<div class="form-group">
+									<label for="selectHabilitado" class="col-xs-6 col-sm-2 col-md-1 col-lg-2 control-label">Habilitado:</label>
+									<div class="col-xs-6 col-sm-10 col-md-11 col-lg-10">
+										<select name="selectHabilitado" id="selectHabilitado" class="form-control">
+											<option value=""></option>
+											<option value="prog-estruturada">Programação Estruturada</option>
+											<option value="poo">Programaçaõ Orientada a Objetos</option>
+											<option value="redes">Redes de Computadores</option>
+											<option value="c#">Programação Comercial</option>
+											<option value="so">Sistemas Operacionais</option>
+										</select>
+									</div>
+								</div>
+
+								<div class="pull-right">
+									<button type="button" class="btn btn-success">Cadastrar</button>
+									<a href="javascript:window.history.go(-1)"><button type="button" class="btn btn-warning">Cancelar</button></a>
+								</div>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!--<div class="wrapper" role="main">
 		<div class="container container-2">
 			<div class="row affix-row">
 				<div id="menu" class="col-xs-12 col-sm-3 col-md-2 affix-sidebar">
@@ -185,21 +300,7 @@
 				</div>
 			</div>
 		</div>
-	</div>
-
-	<footer>
-		<div class="container container-2">
-			<div class="row">
-				<div class="col-xs-12 col-sm-3 col-md-6">
-					<h5><p>Anhanguera Educacional Faculdade de Negócios e Tecnologia da Informação - FACNET<br />Fone: (61) 0000-0000 Fax: (61) 0000-0000<br />Taguatinga-DF</p></h5>
-				</div>
-						
-				<div id="logoFooter" class="col-xs-12 col-sm-3 col-md-3 col-md-offset-3 col-md-3">
-					<h2>Logo</h2>
-				</div>
-			</div>
-		</div>
-	</footer>
+	</div>-->
 
 	<footer class="navbar navbar-fixed-bottom">
 		<div class="copyright">
@@ -215,15 +316,12 @@
 
 	<script src="../js/jquery.min.js"></script>
 	<script src="../js/bootstrap.min.js"></script>
-	<script src="../js/bootstrap-datepicker.js"></script>
+	<script src="../js/bootstrap-datetimepicker.min.js"></script>
 
 	<script type="text/javascript">
-		$(function(){
-			$('.input-group.date').datepicker({
-				format: "dd/mm/yyyy",
-				language: "pt-BR",
-				autoclose: true,
-				todayHighlight: true
+		$(document).ready(function() {
+			$('#datepicker').datetimepicker({
+				language: 'pt-BR';
 			});
 		});
 	</script>
