580eb634a0e69028f2f0ea5c3f3b9325bd39cbe6
Reuel <reuel_sc@live.com>
2014-09-16 07:09:08 -0300

    inserido aviso de cadastro de usuário realizado com sucesso

--- coordenador/cadastroUsuario.php
+++ coordenador/cadastroUsuario.php
@@ -160,7 +160,22 @@
 								</div>
 
 								<div class="pull-right">
-									<button type="button" class="btn btn-success">Cadastrar</button>
+									<button type="button" class="btn btn-success" data-toggle="modal" data-target="#user-success">Cadastrar</button>
+										<div class="modal fade" id="user-success" tabindex="-1" role="dialog" aria-labelledby="user-success" aria-hidden="true">
+											<div class="modal-dialog modal">
+												<div class="modal-content">
+													<div class="modal-header">
+														<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Sair</span></button>
+													</div>
+													<div class="modal-body">
+														<p>Usuário cadastrado com sucesso!</p>
+													</div>
+													<div class="modal-footer">
+														<button type="button" class="btn btn-primary" data-dismiss="modal">Ok</button>
+													</div>
+												</div>
+											</div>
+										</div>
 									<a href="javascript:window.history.go(-1)"><button type="button" class="btn btn-warning">Cancelar</button></a>
 								</div>
 							</div>
@@ -170,158 +185,6 @@
 			</div>
 		</div>
 	</div>
-
-	<!--<div class="wrapper" role="main">
-		<div class="container container-fluid">
-			<div class="row affix-row">
-				<div id="menu" class="col-xs-12 col-sm-3 col-md-2 affix-sidebar">
-					<div class="sidebar-nav">
-						<div class="navbar navbar-default">
-							<div id="navbar-collapse-1" class="navbar-collapse collapse">
-								<ul class="nav navbar-nav">
-									<li class="active">
-										<a href="#" data-toggle="collapse"><h4>Menu</h4></a>
-									</li>
-
-									<li>
-										<a href="#" data-toggle="collapse" data-target="#toggleSidebar" data-parent="#sidenav01" class="collapsed">
-											<span>Submenu 1 <span class="caret pull-right"></span></span>
-										</a>
-
-										<div class="collapse" id="toggleSidebar" style="height: 0px;">
-											<ul class="nav nav-list">
-												<li><a href="#">Submenu 1.1</a></li>
-												<li><a href="#">Submenu 1.2</a></li>
-												<li><a href="#">Submenu 1.3</a></li>
-											</ul>
-										</div>
-									</li>
-
-									<li>
-										<a href="#" data-toggle="collapse" data-target="#toggleSidebar2" data-parent="#sidenav02" class="collapsed">
-											<span>Submenu 2 <span class="caret pull-right"></span></span>
-										</a>
-
-										<div class="collapse" id="toggleSidebar2" style="height: 0px;">
-											<ul class="nav nav-list">
-												<li><a href="#">Submenu 2.1</a></li>
-												<li><a href="#">Submenu 2.2</a></li>
-												<li><a href="#">Submenu 2.3</a></li>
-											</ul>
-										</div>
-									</li>
-								</ul>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<div id="conteudo" class="col-xs-12 col-sm-9 col-md-10 affix-content">
-					<div class="container container-fluid">
-						<div class="page-header">
-							<h3><span class="glyphicon glyphicon-th-list"></span> Cadastro de Usuário</h3>
-						</div>
-
-						<form method="post" action="" class="form-horizontal" role="form">
-							<div class="row">
-								<div class="col-md-8">
-									<div class="form-group">
-										<label for="inputName" class="col-xs-6 col-sm-2 col-md-1 col-lg-2 control-label">Nome:</label>
-										<div class="col-xs-6 col-sm-10 col-md-11 col-lg-10">
-											<input type="text" class="form-control" id="inputName" required>
-										</div>
-									</div>
-
-									<div class="form-group">
-										<label for="radioPerfil" class="col-xs-6 col-sm-2 col-md-1 col-lg-2 control-label">Perfil:</label>
-										<div class="col-xs-6 col-sm-10 col-md-11 col-lg-10">
-											<label class="radio-inline">
-												<input type="radio" name="optionPerfil" id="optionProf" value="professor">Professor</input>
-											</label>
-
-											<label class="radio-inline">
-												<input type="radio" name="optionPerfil" id="optionCoord" value="coordenador">Coordenador</input>
-											</label>
-
-											<label class="radio-inline">
-												<input type="radio" name="optionPerfil" id="optionSecret" value="secretaria">Secretária</input>
-											</label>
-										</div>
-									</div>
-
-									<div class="form-group">
-										<label for="inputCPF" class="col-xs-6 col-sm-2 col-md-1 col-lg-2 control-label">CPF:</label>
-										<div class="col-xs-6 col-sm-10 col-md-11 col-lg-10">
-											<input type="text" class="form-control" id="inputCPF" required>
-										</div>
-									</div>
-
-									<div class="form-group">
-										<label for="inputNasc" class="col-xs-6 col-sm-2 col-md-1 col-lg-2 control-label">Data de Nascimento:</label>
-										<div class="col-xs-6 col-sm-10 col-md-11 col-lg-10">
-											<input type="text" class="form-control" id="inputNasc" required>
-										</div>
-									</div>
-
-									<div class="form-group">
-										<label for="inputPhone" class="col-xs-6 col-sm-2 col-md-1 col-lg-2 control-label">Telefone:</label>
-										<div class="col-xs-6 col-sm-10 col-md-11 col-lg-10">
-											<input type="text" class="form-control" id="inputPhone" required>
-										</div>
-									</div>
-
-									<div class="form-group">
-										<label for="inputEndereco" class="col-xs-6 col-sm-2 col-md-1 col-lg-2 control-label">Endereço:</label>
-										<div class="col-xs-6-col-sm-10 col-md-11 col-lg-10">
-											<input type="text" class="form-control" id="inputEndereco" required>
-										</div>
-									</div>
-
-									<div class="form-group">
-										<label for="inputComplemento" class="col-xs-6 col-sm-2 col-md-1 col-lg-2 control-label">Complemento:</label>
-										<div class="col-xs-6-col-sm-10 col-md-11 col-lg-10">
-											<input type="text" class="form-control" id="inputComplemento" required>
-										</div>
-									</div>
-
-									<div class="form-group">
-										<label for="inputCidade" class="col-xs-6 col-sm-2 col-md-1 col-lg-2 control-label">Cidade:</label>
-										<div class="col-xs-6-col-sm-10 col-md-11 col-lg-10">
-											<input type="text" class="form-control" id="inputCidade" required>
-										</div>
-									</div>
-
-									<div class="form-group">
-										<label for="optionEstado" class="col-xs-6 col-sm-2 col-md-1 col-lg-2 control-label">Estado:</label>
-										<div class="col-xs-6 col-sm-10 col-md-11 col-lg-10">
-											<select name="selectEstado" id="selectEstado" class="form-control" required>
-												<option></option>
-												<option>AM</option>
-												<option>PA</option>
-												<option>BA</option>
-												<option>TO</option>
-												<option>DF</option>
-												<option>MA</option>
-												<option>CE</option>
-												<option>RJ</option>
-												<option>SP</option>
-											</select>
-										</div>
-									</div>
-
-									<div class="pull-right">
-										<button type="button" class="btn btn-success">Cadastrar</button>
-										<button type="button" class="btn btn-warning">Editar</button>
-										<button type="button" class="btn btn-danger">Excluir</button>
-									</div>
-								</div>
-							</div>
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>-->
 
 	<footer class="navbar navbar-fixed-bottom">
 		<div class="copyright">
